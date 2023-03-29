8b395d3fe865ff6f5d7d08b9b19f42798f564c66
Vasyl <vasiliy.vs45@gmail.com>
2023-03-29 06:36:53 +0200

    задача на фильмы

--- js/script.js
+++ js/script.js
@@ -1,65 +1,23 @@
 
-// let num = 5;
-// while ( num <= 10 ){
-//     console.log(num);
-//     num++;
-// }
+const numberOfFilms = +prompt('Сколько фильмов вы смотрели?', '');
 
-// for ( let i = 5; i <= 10; i++){
-//     console.log(num);
-//     num++;
-// }
+const personalMovieDB = {
+      const: numberOfFilms,
+      movies: {},
+      actors: {},
+      geners: [],
+      privat: false
+};
 
-// do {
-//     console.log(num);
-//     num++;
-// } while ( num <= 10);
-
-let numFirstExercise = 5;
-for ( let i = 5; i <= 10; i++){
-    console.log(numFirstExercise);
-    numFirstExercise++;
-}
-
-
-let numSecondExercise = 20;
-for ( let i = 0; i < 20; i++){
-    console.log(numSecondExercise);
-    numSecondExercise--;
-    if (numSecondExercise == 13 ) break;
-}
-
-//let number = 0;
-for ( let i = 1; i <= 10; i++){
-    if ( i % 2 == 0){
-        console.log(`${i}`); 
-    }
-}
-
-
-for (let i = 2; i <= 16; i++) {
-    if (i % 2 === 0) {
-        continue;
+for (let i = 0; i < 2; i++){
+    const a = prompt('один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько его оцените?', '');
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
     } else {
-        console.log(i);
+        console.log('error');
+        i--;
     }
 }
-
-let b = 2;
-while ( b <= 16){
-    if ( b % 2 === 0){
-        b++;
-        continue
-    }else {
-        console.log(b);
-    }
-    b++;
-}
-
-
-const arrNumbers = [];
-    for (let i = 5; i <= 10; i++) {
-        arrNumbers[i - 5] = i;
-    }
-    console.log(arrNumbers);
-    return arrNumbers;
+console.log(personalMovieDB);
